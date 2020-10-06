7a7afd078feda7ccf9aeca136d338e527a86d02e
3lviend <henryss_10@yahoo.com>
2020-10-06 14:38:54 +0700

    counter cache

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -1,4 +1,4 @@
 class ForumPost < ApplicationRecord
 	belongs_to :user
-	belongs_to :forum_thread, counter_cache: true
+	belongs_to :forum_thread,counter_cache: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_043103) do
+ActiveRecord::Schema.define(version: 2020_10_06_072747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_10_06_043103) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "forum_posts_count", default: 0
   end
 
   create_table "users", force: :cascade do |t|
