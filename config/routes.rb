9ca1614ece2e600377cc6eb031313519fe5b9910
3lviend <henryss_10@yahoo.com>
2020-10-06 15:47:56 +0700

    form_thread_new

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "forum_threads#index"
 
-  resources :forum_threads, only: [:show]
+  resources :forum_threads, only: [:show, :new]
 end
