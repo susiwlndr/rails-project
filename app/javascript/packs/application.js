35a20ad82499025313311c83f556237b8d7cb9a7
3lviend <henryss_10@yahoo.com>
2020-10-07 08:51:06 +0700

    navbar menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,15 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import "jquery"
+import "bootstrap-sprockets"
+
+//= require jquery
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+// require_tree .
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
